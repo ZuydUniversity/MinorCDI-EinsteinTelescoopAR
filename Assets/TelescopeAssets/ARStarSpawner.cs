be74213b28987e78dd5d703e915153ac2b4ce998
Lucarius13 <56791696+TrueProvidence@users.noreply.github.com>
2025-09-30 12:02:45 +0200

    fix(star): added telescope import flattened spawn area

--- Assets/TelescopeAssets/ARStarSpawner.cs
+++ Assets/TelescopeAssets/ARStarSpawner.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class ARStarSpawner : MonoBehaviour
 {
     public GameObject prefabToSpawn;
+    public GameObject Telescope;
     public Vector3 spawnPosition;
     private Vector3 spawnPos;
     public int amount = 40;
-    public float minDistance = 0.3f;
-    public float maxDistance = 1.0f;
+    public float minDistance = 0.4f;
+    public float maxDistance = 1.2f;
 
     public void SpawnObject()
     {
@@ -15,15 +16,21 @@ public class ARStarSpawner : MonoBehaviour
         int count = amount;
         while (count > 0)
         {
-            // Spawn 1 meter in front of the AR camera
+            // Spawn 1 meter in front of the AR camera if no spawnPosition is given
             Camera cam = Camera.main;
-            if (spawnPosition == null)
+            if (Telescope == null)
             {
                 spawnPosition = cam.transform.position + cam.transform.forward * 1.0f;
             }
+            else { spawnPosition = Telescope.transform.position + new Vector3(0, 1.0f, 0); }
             count -= 1;
-            spawnPos = spawnPosition + Random.onUnitSphere * Random.Range(minDistance, maxDistance);
-            if (spawnPos.y < 0){ spawnPos.y = -spawnPos.y; }
+
+            Vector3 dir = Random.onUnitSphere;
+            dir = new Vector3(dir.x, dir.y * 0.5f, dir.z);
+            dir.Normalize();
+            spawnPos = spawnPosition + dir * Random.Range(minDistance, maxDistance);
+            if (spawnPos.y < 0) { spawnPos.y = -spawnPos.y; }
+
             GameObject star = Instantiate(prefabToSpawn, spawnPos, Quaternion.identity);
             float scale = Random.Range(.2f, 1.2f);
             star.transform.localScale *= scale;
