50d6844cd7ec4c4dfbeb6453ba8e561e2ded079f
Milanh3h3 <149052315+Milanh3h3@users.noreply.github.com>
2025-10-26 10:40:25 +0100

    feat(lever): added lever to toggle showwaves and removed ui button

---
+++ Assets/Scripts/Above Ground/ClickableObject.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class ClickableObject : MonoBehaviour
+{
+    /// <summary>
+    /// title for text canvas for showing information
+    /// </summary>
+    public string title;
+    /// <summary>
+    /// description for text canvas for showing information
+    /// </summary>
+    public string description;
+
+    /// <summary>
+    /// Bool if clickable object is a lever. If it is a lever it does not show information but plays its animation instead.
+    /// </summary>
+    public bool isLever = false;
+    /// <summary>
+    /// bool if lever is on. used only if object is lever. used for animation
+    /// </summary>
+    private bool isOn = false;
+    /// <summary>
+    /// animator for lever anitmation
+    /// </summary>
+    public Animator leverAnimator;
+    /// <summary>
+    /// bool if lever is on. used only if object is lever. used for animation
+    /// </summary>
+    public string toggleParameter = "IsOn";
+    /// <summary>
+    /// Id to recognise which lever it is so which function should run if the lever is toggled
+    /// </summary>
+    public int Id;
+    /// <summary>
+    /// Root of starspawner used to toggle showwaves
+    /// </summary>
+    public GameObject StarSpawnerRoot;
+    /// <summary>
+    /// Toggles lever, use this to add functionality
+    /// </summary>
+    public void ToggleLever()
+    {
+        isOn = !isOn;
+        Debug.Log("Lever toggled: " + (isOn ? "ON" : "OFF"));
+
+        if (leverAnimator != null)
+            leverAnimator.SetBool(toggleParameter, isOn);
+
+        if (isOn)
+        {
+            leverAnimator.SetFloat("Speed", 1f);
+            leverAnimator.Play("Lever_Animation", 0, 0f);
+            //Id == 0 for Showwaves lever
+            if (Id == 0) { StarSpawnerRoot.GetComponent<StarSpawner>().StartAnimation(); }
+        }
+        else
+        {
+            // Animation in reverse
+            leverAnimator.SetFloat("Speed", -1f);
+            leverAnimator.Play("Lever_Animation", 0, 1f);
+            //Id == 0 for Showwaves lever
+            if (Id == 0) { StarSpawnerRoot.GetComponent<StarSpawner>().StopAnimation(); }
+        }
+
+    }
+}
