62188d8c18b555a88a728deb06d9f7c6ddbf7a16
Mineplay5780 <tristanfranssen@gmail.com>
2025-10-30 23:32:52 +0100

    feat(scanner): made scanner react when image is scanned

--- Assets/Scripts/Lift/QRScanner.cs
+++ Assets/Scripts/Lift/QRScanner.cs
@@ -26,11 +26,20 @@ public class QRScanner : MonoBehaviour
     /// <summary>
     /// How long to show the checkmark.
     /// </summary>
-    public float checkmarkDisplayTim = 1.5f;
+    public float checkmarkDisplayTime = 1.5f;
     /// <summary>
     /// The time it takes for the checkmark to fade out.
     /// </summary>
     public float fadeDuration = 0.5f;
+
+    /// <summary>
+    /// When enabled it resets all elements used by the scanner.
+    /// </summary>
+    void OnEnable() {
+        boxFrame.gameObject.SetActive(true);
+        scanLine.gameObject.SetActive(true);
+        checkmark.gameObject.SetActive(false);
+    }
 
     /// <summary>
     /// Moves scan line up and down.
@@ -40,5 +49,37 @@ public class QRScanner : MonoBehaviour
         RectTransform canvasRect = gameObject.GetComponent<RectTransform>();    
         float scanPosition = Mathf.PingPong(Time.time * scanSpeed, 1f);
         scanLine.transform.position = new Vector2(0, Mathf.Lerp(0, canvasRect.rect.height, scanPosition));
+    }
+
+    /// <summary>
+    /// When scan succeeds show checkmark.
+    /// </summary>
+    public void OnScanSuccess() {
+        StartCoroutine(ShowCheckmark());
+    }
+
+    /// <summary>
+    /// Temporary shows checkmark.
+    /// </summary>
+    private IEnumerator ShowCheckmark() {
+        boxFrame.gameObject.SetActive(false);
+        scanLine.gameObject.SetActive(false);
+        checkmark.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(checkmarkDisplayTime);
+
+        float time = 0;
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+
+            Color checkmarkColor = checkmark.color;
+            checkmarkColor.a = Mathf.Lerp(1, 0, time / fadeDuration);
+            checkmark.color = checkmarkColor;
+
+            yield return null;
+        }
+
+        gameObject.SetActive(false);
     }
 }
