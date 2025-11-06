f244b059158443778b72124fcfa00865eb1648d7
David Beljaars <davidbel.zakelijk@gmail.com>
2025-11-06 20:02:28 +0100

    feat(functionality): added new modal for lever tutorial bit, also added script that shows the modal corresponding to the group. It measures if the player is in the set range and then shows the modal once

--- Assets/Scripts/Tutorial/ProximityTrigger.cs
+++ Assets/Scripts/Tutorial/ProximityTrigger.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 public class ProximityTrigger : MonoBehaviour
 {
     public Transform player;
+    public GameObject modalPanel;
+    private bool hasTriggered = false;
 
     void Update()
     {
         if (!player) player = Camera.main.transform;
+        if (hasTriggered) return;
 
         float distance = Vector3.Distance(transform.position, player.position);
         if (distance < 2f)
         {
-            Debug.Log("Player binnen triggerafstand!");
+            hasTriggered = true;
+            if (modalPanel != null)
+            {
+                modalPanel.SetActive(true);
+            }
+            else
+            {
+                Debug.LogWarning("Geen modalPanel gekoppeld aan " + name);
+            }
         }
     }
 }
