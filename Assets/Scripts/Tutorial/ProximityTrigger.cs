de9fdaf8ae9d101d0bab852430ebe77435eb9d63
David Beljaars <davidbel.zakelijk@gmail.com>
2025-11-07 09:43:54 +0100

    fix(comment): added comment ot the proximity function

--- Assets/Scripts/Tutorial/ProximityTrigger.cs
+++ Assets/Scripts/Tutorial/ProximityTrigger.cs
@@ -6,10 +6,23 @@ public class ProximityTrigger : MonoBehaviour
     public GameObject modalPanel;
     private bool hasTriggered = false;
 
+    /// <summary>
+    /// Checks if the player is within a certain distance. If that is the case show the selected modal
+    /// </summary>
+    /// <param name="player">Player transform. Puts the main camera in player.</param>
+    /// <param name="modalPanel">The activated modal once the player reaches the distance.</param>
+
     void Update()
     {
-        if (!player) player = Camera.main.transform;
-        if (hasTriggered) return;
+        if (!player)
+        {
+            player = Camera.main.transform;
+        } 
+
+        if (hasTriggered)
+        {
+            return;
+        } 
 
         float distance = Vector3.Distance(transform.position, player.position);
         if (distance < 2f)
