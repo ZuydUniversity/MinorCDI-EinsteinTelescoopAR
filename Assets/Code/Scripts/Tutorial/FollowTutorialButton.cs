1ecb1e188cb45832f50ee6f454fdb2f78ff91e16
David Beljaars <davidbel.zakelijk@gmail.com>
2025-11-10 15:04:13 +0100

    fix(alignment): fixed the aligment in tutorial scene

--- Assets/Code/Scripts/Tutorial/FollowTutorialButton.cs
+++ Assets/Code/Scripts/Tutorial/FollowTutorialButton.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 /// <summary>
 /// Used to determine if to load the tutorial scene.
@@ -12,11 +13,23 @@ public class FollowTutorialButton : MonoBehaviour
     public string tutorialScene = "TutorialScene";
 
     /// <summary>
+    /// Loads the tutorial on click.
+    /// </summary>
+    public void LoadTutorialOnClick() 
+    {
+        StartCoroutine(LoadTutorial());
+    }
+
+    /// <summary>
     /// Loads the tutorial scene and disables the Lift object in the MainScene.
     /// </summary>
-    public void LoadTutorial() 
+    private IEnumerator LoadTutorial() 
     {
-        SceneManager.LoadSceneAsync(tutorialScene, LoadSceneMode.Additive);
+        AsyncOperation loadSceneOperation = SceneManager.LoadSceneAsync(tutorialScene, LoadSceneMode.Additive);
+        while (!loadSceneOperation.isDone)
+        {
+            yield return null;
+        }
 
         GameObject lift = FindFirstObjectByType<LiftSceneLoader>()?.gameObject;
         if (lift != null)
